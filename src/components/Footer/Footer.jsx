474f2585b56f9dfa65080d3451e836afd1e1aa13
Anthony Rossa <anthonyr.work@proton.me>
2026-08-29 20:31:30 -0300

    change footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,10 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>This is a base template for development, not a finished product.</p>
-      <p>Developed by Anthony Rossa</p>
+      <p className="footer__info">This is a base template for development, not a finished product.</p>
+      <div className="footer__author">
+        <p className="footer__author-name">Developed by Anthony Rossa</p>
+      </div>
     </footer>
   );
 }
